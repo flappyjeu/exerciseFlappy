6def82593674816eb94fe72ca5313a84139f2553
TiMOUNE05 <152992587+TiMOUNE05@users.noreply.github.com>
2024-02-22 11:56:05 -0500

    Champignon

--- Assets/ControlleFlappy.cs
+++ Assets/ControlleFlappy.cs
@@ -7,6 +7,11 @@ public class ControlleFlappy : MonoBehaviour
 
     public float vitesseY;
     public float vitesseX;
+    public Sprite flappyBlesse;
+    public Sprite flappyGuerit;
+    public GameObject PieceOr;
+    public GameObject PackVie;
+    public GameObject Champignon;
     // Start is called before the first frame update
     void Start()
     {
@@ -39,4 +44,56 @@ public class ControlleFlappy : MonoBehaviour
 
         GetComponent<Rigidbody2D>().velocity = new Vector2(vitesseX, vitesseY);
     }
+
+    void OnCollisionEnter2D(Collision2D infoCollision)
+    {
+        //si l'objet flappy touche une colonne le sprite change
+        if (infoCollision.gameObject.name == "Colonne")
+        {
+
+            GetComponent<SpriteRenderer>().sprite = flappyBlesse;
+
+        }
+
+        else if (infoCollision.gameObject.name == "PieceOr")
+        {
+            infoCollision.gameObject.SetActive(false);
+            Invoke("ReactiverPiece", 7f);
+            float valeurAleatoireY = Random.Range(-1, 1);
+            infoCollision.gameObject.transform.position = new Vector2(infoCollision.gameObject.transform.position.x, valeurAleatoireY);
+        }
+
+        else if (infoCollision.gameObject.name == "PackVie")
+        {
+            infoCollision.gameObject.SetActive(false);
+            Invoke("ReactiverPackVie", 7f);
+            GetComponent<SpriteRenderer>().sprite = flappyGuerit;
+        }
+
+        else if (infoCollision.gameObject.name == "Champignon")
+        {
+            infoCollision.gameObject.SetActive(false);
+            Invoke("ReactiverChampignon", 7f);
+            transform.localScale *= 2f;
+        }
     }
+
+    void ReactiverPiece()
+    {
+        PieceOr.SetActive(true);
+    }
+
+    void ReactiverPackVie()
+    {
+        PackVie.SetActive(true);
+    }
+
+    void ReactiverChampignon()
+    {
+        Champignon.SetActive(true);
+        transform.localScale /= 2f;
+    }
+
+
+
+}
