9d5dace3427190f853e5737cd80ead2c2e20f6a0
TiMOUNE05 <152992587+TiMOUNE05@users.noreply.github.com>
2024-02-29 13:49:19 -0500

    Flappy Partie 4

--- Assets/ControlleFlappy.cs
+++ Assets/ControlleFlappy.cs
@@ -12,6 +12,15 @@ public class ControlleFlappy : MonoBehaviour
     public GameObject PieceOr;
     public GameObject PackVie;
     public GameObject Champignon;
+    public AudioSource musique;
+    public AudioClip SonCol;
+    public AudioClip SonOr;
+    public AudioClip SonPack;
+    public AudioClip SonChamp;
+    public AudioClip SonFinPartie;
+    bool blesse;
+    bool partieTerminee;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -50,8 +59,12 @@ public class ControlleFlappy : MonoBehaviour
         //si l'objet flappy touche une colonne le sprite change
         if (infoCollision.gameObject.name == "Colonne")
         {
-
+            blesse = true;
+            partieTerminee = true;
+            GetComponent<Rigidbody2D>().freezeRotation = false;
+            GetComponent<Collider2D>().enabled = false;
             GetComponent<SpriteRenderer>().sprite = flappyBlesse;
+            GetComponent<AudioSource>().PlayOneShot(SonCol, 1f);
 
         }
 
@@ -61,6 +74,7 @@ public class ControlleFlappy : MonoBehaviour
             Invoke("ReactiverPiece", 7f);
             float valeurAleatoireY = Random.Range(-1, 1);
             infoCollision.gameObject.transform.position = new Vector2(infoCollision.gameObject.transform.position.x, valeurAleatoireY);
+            GetComponent<AudioSource>().PlayOneShot(SonOr, 1f);
         }
 
         else if (infoCollision.gameObject.name == "PackVie")
@@ -68,6 +82,7 @@ public class ControlleFlappy : MonoBehaviour
             infoCollision.gameObject.SetActive(false);
             Invoke("ReactiverPackVie", 7f);
             GetComponent<SpriteRenderer>().sprite = flappyGuerit;
+            GetComponent<AudioSource>().PlayOneShot(SonPack, 1f);
         }
 
         else if (infoCollision.gameObject.name == "Champignon")
@@ -75,6 +90,7 @@ public class ControlleFlappy : MonoBehaviour
             infoCollision.gameObject.SetActive(false);
             Invoke("ReactiverChampignon", 7f);
             transform.localScale *= 2f;
+            GetComponent<AudioSource>().PlayOneShot(SonChamp, 1f);
         }
     }
 
