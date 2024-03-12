f1a4c6de75d2410685111b4b40a7b327495d24ed
TiMOUNE05 <152992587+TiMOUNE05@users.noreply.github.com>
2024-03-12 12:26:19 -0400

    Flappy font

--- Assets/ControlleFlappy.cs
+++ Assets/ControlleFlappy.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class ControlleFlappy : MonoBehaviour
 {
@@ -23,12 +24,17 @@ public class ControlleFlappy : MonoBehaviour
     public AudioClip SonFinPartie;
     bool blesse;
     bool partieTerminer;
+    public TextMeshProUGUI score;
+    public TextMeshProUGUI texteFin;
+    int compteur = 0;
+
     
 
     // Start is called before the first frame update
     void Start()
     {
-        
+       // texteFin.gameObject.SetActive(false);
+
     }
 
     // Update is called once per frame
@@ -89,6 +95,8 @@ public class ControlleFlappy : MonoBehaviour
         //si l'objet flappy touche une colonne le sprite change
         if (infoCollision.gameObject.name == "Colonne")
         {
+            compteur -= 5; //enleve 5 points chaque fois que flappy touche une colonne
+            score.text = compteur.ToString();
 
             {
                 if (blesse == false)
@@ -116,6 +124,8 @@ public class ControlleFlappy : MonoBehaviour
         {
             infoCollision.gameObject.SetActive(false);
             Invoke("ReactiverPiece", 7f);
+            compteur += 5; //ajoute 5 points
+            score.text = compteur.ToString();
             float valeurAleatoireY = Random.Range(-1, 1);
             infoCollision.gameObject.transform.position = new Vector2(infoCollision.gameObject.transform.position.x, valeurAleatoireY);
             GetComponent<AudioSource>().PlayOneShot(SonOr, 1f);
@@ -125,6 +135,8 @@ public class ControlleFlappy : MonoBehaviour
         {
             infoCollision.gameObject.SetActive(false);
             Invoke("ReactiverPackVie", 7f);
+            compteur += 5; //ajoute 5 points
+            score.text = compteur.ToString();
             GetComponent<SpriteRenderer>().sprite = flappyGuerit;
             GetComponent<AudioSource>().PlayOneShot(SonPack, 1f);
         }
@@ -133,6 +145,8 @@ public class ControlleFlappy : MonoBehaviour
         {
             infoCollision.gameObject.SetActive(false);
             Invoke("ReactiverChampignon", 7f);
+            compteur += 10; //ajoute 5 points
+            score.text = compteur.ToString();
             transform.localScale *= 2f;
             GetComponent<AudioSource>().PlayOneShot(SonChamp, 1f);
         }
@@ -156,7 +170,7 @@ public class ControlleFlappy : MonoBehaviour
 
     void relancerScene()
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene("exerciseFlappy");
     }
 
 }
